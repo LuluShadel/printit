f7d50088af144a8a9d1f723a628eb077e2f79d1b
Lucile THIEULIN <lucile.thieulin96@gmail.com>
2023-07-21 17:14:06 +0200

    ajout des events listener sur les fleches, ajout de la fonction pour ajouter les images et textes

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,50 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+let i =0
+
+// ecoute du click sur la fleche de gauche 
+let arrowLeft = document.querySelector(".arrow_left");
+arrowLeft.addEventListener("click", () => {
+	i--
+	console.log(i)
+	caroussel()
+})
+
+
+//ecoute du click sur la fleche de droite 
+let arrowRight = document.querySelector(".arrow_right");
+arrowRight.addEventListener("click", () => {
+	i++
+	console.log(i)
+	caroussel()
+	
+
+})
+
+
+
+
+function caroussel (image,tagLine){
+
+let carousselImage = document.querySelector(".banner-img")
+carousselImage.setAttribute("src","./assets/images/slideshow/"+slides[i].image)
+
+
+let carousselText = document.querySelector("#banner p")
+carousselText.innerHTML=slides[i].tagLine
+
+}
+
+
+
+	
+
+
+ 
+
+
+
+
+
